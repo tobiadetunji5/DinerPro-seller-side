3fa4bba0d8d8787288a28e7ddb1e1fa75853c46d
B-mitchell <mitchellonuorah77@gmail.com>
2023-07-15 13:42:32 +0100

    little updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import React from "react";
 import MainLayout from "./components/main_layout/MainLayout";
 import "./globals.css";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-screen">
-      <body className={inter.className + " h-full"}>
+      <body className={"h-full"}>
         <main className="h-full">
           <div className="h-full">
             <MainLayout>{children}</MainLayout>
